3ad4c79a67e52003d8f9ec77c7569f3a15ff72cb
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2023-12-07 18:09:49 +0100

    dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateProject from './components/createProject'
+
+export default function Dashboard() {
+  return (
+    <main>
+      <h1>Dashboard</h1>
+    
+    <CreateProject />
+    </main>
+  )
+}
