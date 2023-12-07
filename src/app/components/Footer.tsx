69d55926ef4d4a4491f3b9bff3e899a1551c0512
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2023-12-07 18:05:18 +0100

    add footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+
+export default function Footer() {
+  return (
+    <footer className="flex justify-center items-center h-16 bg-black text-white">
+      <p className="text-center">
+        Made with ❤️ 
+        </p>
+    </footer>
+
+  )
+}
