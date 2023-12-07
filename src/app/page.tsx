4c16bb84fc983da74581cbf33cc065b5df4e245f
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2023-12-07 18:11:22 +0100

    home page

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+
+
+
+export async function generateMetadata() {
+  return {
+    title: 'Welcome to my portfolio',
+    description: 'Home page',
+  }
+}
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      
+    </main>
+  )
+}
